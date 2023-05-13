ebc3f469c8bfa9be19909b242b9243f1d900de2c
PiyaS.COMP <piyawork52@gmail.com>
2023-05-13 23:37:53 +0700

    D

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./indexedDB.css";
+
+function index() {
+    return(
+        <html>
+            <head><title>Web resource</title></head>
+            <body>
+                <div align ='text-center'> CON </div>
+            </body>
+        </html>
+    );
+}
+
+export default index;
