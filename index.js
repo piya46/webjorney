fd0624573a521d5c68be4b70f3276d3c96fd5924
PiyaS.COMP <piyawork52@gmail.com>
2023-05-14 15:55:37 +0700

    ...

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import "./indexedDB.css";
 
 function index() {
     return(
-        <html>
-            <head><title>Web resource</title></head>
-            <body>
+        // <html>
+        //     <head><title>Web resource</title></head>
+        //     <body>
                 <div align ='text-center'> CON </div>
-            </body>
-        </html>
+        //     </body>
+        // </html>
     );
 }
 
